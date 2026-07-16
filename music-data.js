7de3837b4a87103b6647dfc7e83d2ea62c27649f
kyellnewbie1 <kyellnewbie@gmail.com>
2026-07-16 17:22:06 +0700

    Add files via upload

---
+++ music-data.js
@@ -0,0 +1,13 @@
+// File: music-data.js
+// Masukkan semua data musikmu di dalam array ini.
+// Jika array ini kosong [], maka website juga akan kosong.
+
+const musicDatabase = [
+    {
+        id: "m1",
+        title: "Terhebat",
+        artist: "Hololive ID",
+        cover: "https://files.catbox.moe/eqsw3u.jpg",
+        url: "https://files.catbox.moe/yrn1zm.mp3"
+    }
+];
